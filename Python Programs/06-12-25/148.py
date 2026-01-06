e82be76e7a10ff22d0e8951f7636639560d5558d
ALVF <arindhimar116@gmail.com>
2026-01-06 14:03:01 +0530

    python programs

--- Python Programs/06-12-25/148.py
+++ Python Programs/06-12-25/148.py
@@ -4,16 +4,19 @@ def split_in_two_max(tempNum):
     tempListOfDigits=[int(digit) for digit in tempStr]
     
     tempSum=0
+    maxSum=0
+    if len(tempListOfDigits)<2: return tempNum 
+    for i in range(1,len(tempStr)):
+        firstPart=int(tempStr[:i])
+        secondPart=int(tempStr[i:])
+        
+        tempSum=firstPart+secondPart
+        if tempSum>maxSum:
+            maxSum=tempSum
     
-    for i in range(len(tempListOfDigits)):
-        for j in range(i+1,len(tempListOfDigits)):
-            sumOfPair=tempListOfDigits[i]+tempListOfDigits[j]
-            if sumOfPair>tempSum:
-
-                tempSum=sumOfPair
-    # print(tempListOfDigits[i],tempListOfDigits[j])
-    return tempSum
-
+    return maxSum
+        
+        
 
 number=int(input("Enter a positive integer: "))
 print(split_in_two_max(number))
